50f3e08fef3cee034519520ad0dc4510029af7cf
Ana Nascimento <anacdon@yahoo.com.br>
2021-10-27 16:26:25 -0300

    adicionado show e create

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,6 @@
 class PlayersController < ApplicationController
+  before_action :find, only: :show
+
   def index
     @players = Player.all
   end
@@ -7,9 +9,25 @@ class PlayersController < ApplicationController
     @player = Player.new
   end
 
+  def create
+    @player = Player.new(player_params)
+    if @player.save
+      redirect_to player_path(@player)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
   private
 
   def player_params
     params.require(:player).permit(:name, :age, :position, :price)
+  end
+
+  def find
+    @player = Player.find(params[:id])
   end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,7 @@
 class Player < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :position, presence: true
-  validates :position, inclusion: ['goleiro', 'lateral esquerdo', 'lateral direito', 'zagueiro', 'volante', 'meia', 'atacante']
+  validates :position, inclusion: ['Goleiro', 'Lateral esquerdo', 'Lateral direito', 'Zagueiro', 'Volante', 'Meia', 'Atacante']
   validates :price, presence: true, numericality: { greather_than: 0}
   validates :age, presence: true, numericality: { greather_than: 0}
 end
