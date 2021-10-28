57b76f6223bfb7d7aae69233f88be040e2994c10
Fabio Oliveira <fabio.oliveira@agricultura.gov.br>
2021-10-28 14:31:03 -0300

    missing deals paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :players, except: :destroy
+  resources :players, except: :destroy do
+    resources :details
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
