41fd4e695d0410893bfec2348ee82910b21ef43e
Fabio Oliveira <fabio.oliveira@agricultura.gov.br>
2021-10-28 14:57:51 -0300

    routes reorganization

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :deals, only: [:new, :create]
   root to: 'pages#home'
-  resources :players, except: :destroy do
-    resources :details
-  end
+  resources :players, except: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
