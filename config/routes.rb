a6b5d79d46bfc248e564aee61b74a95451f206a6
Fabio Oliveira <fabio.oliveira@agricultura.gov.br>
2021-10-28 16:18:10 -0300

    deals working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :deals, only: [:new, :create]
+  resources :players, except: :destroy do
+    resources :deals
+  end
+  get "/cart", to: "deals#cart"
+  patch "/sale", to: "deals#sale", as: "sale"
   root to: 'pages#home'
-  resources :players, except: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
