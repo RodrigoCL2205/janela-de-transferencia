01727a6b24921ce39d7290e3e2215b6a1099ac38
Rodrigo Lima <rodrigo.cerqueira@gmail.com>
2021-10-29 17:32:55 -0300

    Include users managers

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   patch "/sale", to: "deals#sale", as: "sale"
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :update, :destroy]
 end
